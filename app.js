ba9ef966f31d85cc74e23f59f0c75112724dc28c
offical-idiot <monkomonko50@gmail.com>
2026-05-03 18:03:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const supabase = window.supabase.createClient(
   "https://pvjdwtgsulrmxamxrwrx.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2amR3dGdzdWxybXhhbXhyd3J4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MzUxMzUsImV4cCI6MjA5MzQxMTEzNX0.2V9YYb8Imqvx8bGJT2pVNwUJnwE_BYYxINf-pcRbCQA"
 );
-window.supabaseClient = window.supabase.createClient(...)
+window.supabaseClient = window.supabase.createClient("https://pvjdwtgsulrmxamxrwrx.supabase.co" "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2amR3dGdzdWxybXhhbXhyd3J4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MzUxMzUsImV4cCI6MjA5MzQxMTEzNX0.2V9YYb8Imqvx8bGJT2pVNwUJnwE_BYYxINf-pcRbCQA")
 window.onload = () => show("home");
 let currentUser = null;
 let role = "guest";
