8c5dd64a67c9d430d434f6e9086ebd6af019c097
offical-idiot <monkomonko50@gmail.com>
2026-05-03 17:57:16 -0500

    Refactor supabase client initialization

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // ---------- SUPABASE ----------
-  const supabase = window.supabase.createClient(
-    "https://pvjdwtgsulrmxamxrwrx.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2amR3dGdzdWxybXhhbXhyd3J4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MzUxMzUsImV4cCI6MjA5MzQxMTEzNX0.2V9YYb8Imqvx8bGJT2pVNwUJnwE_BYYxINf-pcRbCQA"
-  );
+const supabase = window.supabase.createClient(
+  "https://pvjdwtgsulrmxamxrwrx.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2amR3dGdzdWxybXhhbXhyd3J4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MzUxMzUsImV4cCI6MjA5MzQxMTEzNX0.2V9YYb8Imqvx8bGJT2pVNwUJnwE_BYYxINf-pcRbCQA"
+);
 window.onload = () => show("home");
 let currentUser = null;
 let role = "guest";
